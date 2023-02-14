57f8296de30d67a1ecf5c94bf8e755a85e2ab7d0
no3 <meow@no3arch>
2023-02-14 15:14:46 +0100

    jdfks

--- archlinux/postinstall_script.sh
+++ archlinux/postinstall_script.sh
@@ -1,14 +1,39 @@
-               	 install arch linux base meta-packages                    #
+#!/bin/bash
+
+comment=$'\033[0;30m'
+red=$'\033[0;31m'
+green=$'\033[0;32m'
+orange=$'\033[0;33m'
+blue=$'\033[34m'
+purple=$'\033[1;35m'
+boldlightgreen=$'\033[1;36m'
+boldwhite=$'\033[1;37m'
+#eight=$'\033[1;38m' #same as boldbeige
+boldlightgrey=$'\033[1;39m'
+reset=$'\033[0m'
+
+#enable trim to preservce SSD
+sudo systemctl enable fstrim.timer
+sudo systemctl start fstrim.timer
+
+#devices set up (touchpad, mouse)
+sudo cp -R 40-libinput.conf /usr/share/X11/xorg.conf.d/ 
+
+#remove system beep
+sudo cp nobeep.conf /etc/modprobe.d/
+
+###############################################################################
+#              	 install arch linux base meta-packages                    	  #
 ###############################################################################
 
 #meta-package = a package that installs other packages (through the dependencies that he defines)
 
 #core is the minimal package set to define a basic Arch Linux install
-pacman -Syu core
+sudo pacman -Syu core
 
 #base-devel installs gnu utilities, some useful libs like libtool, and other indispensable
 #packages like sudo or gcc
-pacman -Syu base-devel
+sudo pacman -Syu base-devel
 
 
 ###############################################################################
@@ -16,15 +41,31 @@ pacman -Syu base-devel
 ###############################################################################
 
 #install man pages : $ man <cmd> to know how this cmd works, what options does it have
-pacman -Syu man-db 
-
-#install all existent fonts so u have a lot of them in once
-pacman -Syu all-repository-fonts
+sudo pacman -Syu man-db 
 
 #install git
-pacman -Syu git
+sudo pacman -Syu git
 
 #install coding tools
-pacman -Syu valgrind 
+sudo pacman -Syu valgrind 
 
-bumblebee-status sensors2 pasink pasource powerline powerline-fonts pango
+sudo pacman -Syu yay
+
+yay -S zsh zsh-completions zsh-autosuggestions zsh-history-substring-search
+
+yay -S firefox
+
+yay -S discord
+
+yay -S alacritty
+
+yay -S bat
+
+
+
+
+
+
+#bumblebee-status sensors2 pasink pasource powerline powerline-fonts pango
+#install all existent fonts so u have a lot of them in once
+#pacman -Syu all-repository-fonts
