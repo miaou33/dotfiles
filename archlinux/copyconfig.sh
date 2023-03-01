b35dc6d49db83c5615172a2a8b947ad7e443f844
no3 <meow@no3arch>
2023-03-01 10:38:12 +0100

    copyconfig with only necessary copies

--- archlinux/copyconfig.sh
+++ archlinux/copyconfig.sh
@@ -8,12 +8,12 @@ rsync -avH $DOT_PATH/.config/i3/config $BAKUPS_PATH/.config/i3/"config_$(date +'
 cp -v ~/.config/i3/config $DOT_PATH/.config/i3/config
 
 # Picom
-rsync -avH $DOT_PATH/.config/picom/picom.conf $BAKUPS_PATH/.config/picom/"picom.conf_$(date +'%Y-%m-%d_%H-%M-%S').bak"
-cp -v ~/.config/picom/picom.conf $DOT_PATH/.config/picom/picom.conf
+#rsync -avH $DOT_PATH/.config/picom/picom.conf $BAKUPS_PATH/.config/picom/"picom.conf_$(date +'%Y-%m-%d_%H-%M-%S').bak"
+#cp -v ~/.config/picom/picom.conf $DOT_PATH/.config/picom/picom.conf
 
 # Alacritty
-rsync -avH $DOT_PATH/.config/alacritty/alacritty.yml $BAKUPS_PATH/.config/alacritty/"alacritty.yml_$(date +'%Y-%m-%d_%H-%M-%S').bak"
-cp -v ~/.config/alacritty/alacritty.yml $DOT_PATH/.config/alacritty/alacritty.yml
+#rsync -avH $DOT_PATH/.config/alacritty/alacritty.yml $BAKUPS_PATH/.config/alacritty/"alacritty.yml_$(date +'%Y-%m-%d_%H-%M-%S').bak"
+#cp -v ~/.config/alacritty/alacritty.yml $DOT_PATH/.config/alacritty/alacritty.yml
 
 # Polybar
 rsync -avH $DOT_PATH/.config/polybar/launch.sh $BAKUPS_PATH/.config/polybar/"launch.sh_$(date +'%Y-%m-%d_%H-%M-%S').bak"
@@ -22,23 +22,23 @@ rsync -avH $DOT_PATH/.config/polybar/config.ini $BAKUPS_PATH/.config/polybar/"co
 cp -v ~/.config/polybar/config.ini $DOT_PATH/.config/polybar/config.ini
 
 # Zsh
-rsync -avH $DOT_PATH/.zshrc $BAKUPS_PATH/".zshrc_$(date +'%Y-%m-%d_%H-%M-%S').bak"
-cp ~/.zshrc $DOT_PATH/.zshrc
-rsync -avH $DOT_PATH/.p10k.zsh $BAKUPS_PATH/".p10k.zsh_$(date +'%Y-%m-%d_%H-%M-%S').bak"
-cp -v ~/.p10k.zsh $DOT_PATH/.p10k.zsh
+#rsync -avH $DOT_PATH/.zshrc $BAKUPS_PATH/".zshrc_$(date +'%Y-%m-%d_%H-%M-%S').bak"
+#cp ~/.zshrc $DOT_PATH/.zshrc
+#rsync -avH $DOT_PATH/.p10k.zsh $BAKUPS_PATH/".p10k.zsh_$(date +'%Y-%m-%d_%H-%M-%S').bak"
+#cp -v ~/.p10k.zsh $DOT_PATH/.p10k.zsh
 
 # Vim
-rsync -avH $DOT_PATH/.vimrc $BAKUPS_PATH/".vimrc_$(date +'%Y-%m-%d_%H-%M-%S').bak"
-cp -v ~/.vimrc $DOT_PATH/.vimrc
+#rsync -avH $DOT_PATH/.vimrc $BAKUPS_PATH/".vimrc_$(date +'%Y-%m-%d_%H-%M-%S').bak"
+#cp -v ~/.vimrc $DOT_PATH/.vimrc
 
 # Fehbg
-rsync -avH $DOT_PATH/.fehbg $BAKUPS_PATH/".fehbg_$(date +'%Y-%m-%d_%H-%M-%S').bak"
-cp -v ~/.fehbg $DOT_PATH/.fehbg
+#rsync -avH $DOT_PATH/.fehbg $BAKUPS_PATH/".fehbg_$(date +'%Y-%m-%d_%H-%M-%S').bak"
+#cp -v ~/.fehbg $DOT_PATH/.fehbg
 
 # Lightdm
-rsync -avH $DOT_PATH/etc/lightdm/lightdm.conf $BAKUPS_PATH/etc/lightdm/"lightdm.conf_$(date +'%Y-%m-%d_%H-%M-%S').bak"
-sudo cp -v  /etc/lightdm/lightdm.conf $DOT_PATH/etc/lightdm/lightdm.conf
-rsync -avH $DOT_PATH/etc/lightdm/slick-greeter.conf $BAKUPS_PATH/etc/"lightdm/slick-greeter.conf_$(date +'%Y-%m-%d_%H-%M-%S').bak"
-sudo cp -v  /etc/lightdm/slick-greeter.conf $DOT_PATH/etc/lightdm/slick-greeter.conf
+#rsync -avH $DOT_PATH/etc/lightdm/lightdm.conf $BAKUPS_PATH/etc/lightdm/"lightdm.conf_$(date +'%Y-%m-%d_%H-%M-%S').bak"
+#sudo cp -v  /etc/lightdm/lightdm.conf $DOT_PATH/etc/lightdm/lightdm.conf
+#rsync -avH $DOT_PATH/etc/lightdm/slick-greeter.conf $BAKUPS_PATH/etc/"lightdm/slick-greeter.conf_$(date +'%Y-%m-%d_%H-%M-%S').bak"
+#sudo cp -v  /etc/lightdm/slick-greeter.conf $DOT_PATH/etc/lightdm/slick-greeter.conf
 
 
