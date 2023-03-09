69b40ab2808fee63d7a21040980e4df869691f84
no3 <meow@no3arch>
2023-03-09 15:34:30 +0100

    changed config a bit. added nautilus as file manager

--- archlinux/copyconfig.sh
+++ archlinux/copyconfig.sh
@@ -3,41 +3,34 @@
 DOT_PATH=/home/meow/repos/MINE/CONFiiG/archlinux/dotfiles
 BAKUPS_PATH=/home/meow/repos/MINE/CONFiiG/archlinux/dotfilesbakups
 
-#TO CKEK IF OK : option dry ryn
+#/!\ BACKUPPPPPP
+#>>>>> TO CHEK IF OK : option dry ryn
 #rsync -avH --dry-run $DOT_PATH $BAKUPS_PATH/"$(date +'%Y-%m-%d_%H-%M')"
-#rsync -avH $DOT_PATH $BAKUPS_PATH/"$(date +'%Y-%m-%d_%H-%M')"
+rsync -avH $DOT_PATH $BAKUPS_PATH/"$(date +'%Y-%m-%d_%H-%M')"
 
 ### i3CONFIG ###
 cp -v ~/.config/i3/config $DOT_PATH/.config/i3/config
 
-### PICOM ###
-cp -v ~/.config/picom/picom.conf $DOT_PATH/.config/picom/
+### APPS ###
+#cp -v ~/.config/picom/picom.conf $DOT_PATH/.config/picom/
+#cp -v ~/.config/alacritty/alacritty.yml $DOT_PATH/.config/alacritty/
+#cp -v ~/.config/dunst/dunstrc $DOT_PATH/.config/dunst/
+#cp -rv ~/.config/polybar/* $DOT_PATH/.config/polybar/
+#cp -v ~/.config/mimeapps.list $DOT_PATH/.config/mimeapps.list
 
-### ALACRITTY ###
-cp -v ~/.config/alacritty/alacritty.yml $DOT_PATH/.config/alacritty/
-
-### DUNST ###
-cp -v ~/.config/dunst/dunstrc $DOT_PATH/.config/dunst/
-
-### Polybar ###
-cp -rv ~/.config/polybar/* $DOT_PATH/.config/polybar/
-
-### ZSH ###
-cp -v ~/.zshrc $DOT_PATH/.zshrc
-cp -v ~/.p10k.zsh $DOT_PATH/.p10k.zsh
-
-### VIM / NVIM ###
+### SHELL / EDITOR ###
+#cp -v ~/.zshrc $DOT_PATH/.zshrc
+#cp -v ~/.p10k.zsh $DOT_PATH/.p10k.zsh
 #cp -v ~/.vimrc $DOT_PATH/.vimrc
 #cp -v nvimmmmm
 
-### FEHBG ###
+### DISPLAY ###
 #cp -v ~/.fehbg $DOT_PATH/.fehbg
+cp -v ~/monitor_display.sh $DOT_PATH/monitor_display.sh
 
-### LIGHTDM ###
+### SYSTEM ###
 #cp -v  /etc/lightdm/lightdm.conf $DOT_PATH/etc/lightdm/lightdm.conf
 #cp -v  /etc/lightdm/slick-greeter.conf $DOT_PATH/etc/lightdm/slick-greeter.conf
-
-### Accessories (mouse, touchpad, keyboard, ...) ###
 #cp -v /usr/local/sbin/lightdm-setup.sh DOT_PATH/usr/local/sbin/lightdm-setup.sh
 #cp -vR /etc/X11/xorg.conf.d/* DOT_PATH/etc/X11/xorg.conf.d/
 
---
+++ archlinux/dotfiles/monitor_display.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/sh
+
+# The xRandR names of my monitors, including the internal laptop monitor / display
+readonly MON_INTERNAL='eDP-1'
+readonly MON1='HDMI-1'
+
+# The resolutiond of the given xRandR monitors / displays. NOTE: $MON1 and $MON1_FALLBACK are the same display, so only one res is needed
+readonly MON_INTERNAL_RES='2560x1600'
+readonly MON1_RES='1920x1080'
+
+main_mon=''
+sec_mon=''
+
+# Store a count of how many monitors are connected
+mon_count=$(xrandr -q | grep -w 'connected' | wc -l)
+
+# Configure the monitors via xRandR
+config_monitors() {
+    if [[ "$#" -eq "2" ]]; then
+        xrandr --output $MON1 --auto --right-of $MON_INTERNAL
+    fi
+}
