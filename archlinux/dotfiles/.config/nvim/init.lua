f8df457aadae26a50977f689d0ebe82cd59f6238
no3 <meow@no3arch>
2023-05-03 12:45:23 +0200

    highlight yank

--- archlinux/dotfiles/.config/nvim/init.lua
+++ archlinux/dotfiles/.config/nvim/init.lua
@@ -2,6 +2,7 @@
 --------------------------------------------------
 --				KEYMAPS & OPTIONS				--
 --------------------------------------------------
+require ("meow.autocmds")
 require ("meow.keymaps")
 require ("meow.options")
 require ("meow.plugins")
