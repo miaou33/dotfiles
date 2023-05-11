aea2c4979a5beed071c4f365c7fcbfc81245b7d7
no3 <meow@no3arch>
2023-05-11 20:05:03 +0200

    u

--- archlinux/dotfiles/.config/nvim/after/plugin/fugitive.lua
+++ archlinux/dotfiles/.config/nvim/after/plugin/fugitive.lua
@@ -6,7 +6,18 @@ local k = vim.keymap.set
 k('n', '<leader>gs', vim.cmd.Git)
 k('n', '<space>gp', ':Git push<CR>')
 
---cc                      Create a commit.
---ca                      Amend the last commit and edit the message.
---ce                      Amend the last commit without editing the message.
---cw                      Reword the last commit.
+--=		Toggle an inline diff of the file under the cursor.
+
+--s		Stage (add) the file or hunk under the cursor.
+--u		Unstage (reset) the file or hunk under the cursor.
+--U		Unstage everything.
+
+--cc	Create a commit.
+--ca	Amend the last commit and edit the message.
+--ce	Amend the last commit without editing the message.
+--cw	Reword the last commit.
+--crc	Revert the commit under the cursor.
+
+--ri	Perform an interactive rebase.
+--		>Uses ancestor of commit under cursor as upstream if available.
+
--- archlinux/dotfiles/.config/nvim/plugin/packer_compiled.lua
+++ archlinux/dotfiles/.config/nvim/plugin/packer_compiled.lua
@@ -318,22 +318,22 @@ time([[Defining packer_plugins]], false)
 time([[Setup for markdown-preview.nvim]], true)
 try_loadstring("\27LJ\2\n=\0\0\2\0\4\0\0056\0\0\0009\0\1\0005\1\3\0=\1\2\0K\0\1\0\1\2\0\0\rmarkdown\19mkdp_filetypes\6g\bvim\0", "setup", "markdown-preview.nvim")
 time([[Setup for markdown-preview.nvim]], false)
--- Config for: wilder.nvim
-time([[Config for wilder.nvim]], true)
-try_loadstring("\27LJ\2\n\v\0\0\1\0\0\0\1K\0\1\0\0", "config", "wilder.nvim")
-time([[Config for wilder.nvim]], false)
--- Config for: poimandres.nvim
-time([[Config for poimandres.nvim]], true)
-try_loadstring("\27LJ\2\n<\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\15poimandres\frequire\0", "config", "poimandres.nvim")
-time([[Config for poimandres.nvim]], false)
 -- Config for: monochrome.nvim
 time([[Config for monochrome.nvim]], true)
 try_loadstring("\27LJ\2\n\v\0\0\1\0\0\0\1K\0\1\0\0", "config", "monochrome.nvim")
 time([[Config for monochrome.nvim]], false)
+-- Config for: poimandres.nvim
+time([[Config for poimandres.nvim]], true)
+try_loadstring("\27LJ\2\n<\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\15poimandres\frequire\0", "config", "poimandres.nvim")
+time([[Config for poimandres.nvim]], false)
 -- Config for: auto-session
 time([[Config for auto-session]], true)
 try_loadstring("\27LJ\2\n”\1\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\4\0=\3\5\2B\0\2\1K\0\1\0\31auto_session_suppress_dirs\1\5\0\0\a~/\15~/Projects\16~/Downloads\6/\1\0\1\14log_level\nerror\nsetup\17auto-session\frequire\0", "config", "auto-session")
 time([[Config for auto-session]], false)
+-- Config for: wilder.nvim
+time([[Config for wilder.nvim]], true)
+try_loadstring("\27LJ\2\n\v\0\0\1\0\0\0\1K\0\1\0\0", "config", "wilder.nvim")
+time([[Config for wilder.nvim]], false)
 -- Config for: everforest-nvim
 time([[Config for everforest-nvim]], true)
 try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15everforest\frequire\0", "config", "everforest-nvim")
