0639b102f278dcc2e8b8e9af317964308c2aaf02
no3 <meow@no3arch>
2023-05-11 19:41:24 +0200

    um

--- archlinux/dotfiles/.config/nvim/after/plugin/fugitive.lua
+++ archlinux/dotfiles/.config/nvim/after/plugin/fugitive.lua
@@ -6,3 +6,7 @@ local k = vim.keymap.set
 k('n', '<leader>gs', vim.cmd.Git)
 k('n', '<space>gp', ':Git push<CR>')
 
+--cc                      Create a commit.
+--ca                      Amend the last commit and edit the message.
+--ce                      Amend the last commit without editing the message.
+--cw                      Reword the last commit.
