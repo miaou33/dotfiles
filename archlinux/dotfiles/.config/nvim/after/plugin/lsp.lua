63cd5eb3de3130b5adfabbb4e9774af6a421413c
miaou33 <nfauconn@student.42.fr>
2023-05-14 19:05:11 +0200

    removed lsp diag notification messages yurk

--- archlinux/dotfiles/.config/nvim/after/plugin/lsp.lua
+++ archlinux/dotfiles/.config/nvim/after/plugin/lsp.lua
@@ -3,8 +3,8 @@
 --				BORDERS					--
 ------------------------------------------
 
-vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
-vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+--vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
+--vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
 
 local border = {
 	{"🭽", "FloatBorder"},
@@ -81,7 +81,7 @@ vim.diagnostic.config({
 		active = signs, -- show signs
 	},
 	underline = true,
-	update_in_insert = true,
+	update_in_insert = false,
 	severity_sort = false,
 	float = {
 		focusable = true,
@@ -106,26 +106,26 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.s
 ------------------------------------------
 --		DIAG TO MESSAGE AREA			--
 ------------------------------------------
-function PrintDiagnostics(opts, bufnr, line_nr, client_id)
-	bufnr = bufnr or 0
-	line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
-	opts = opts or {['lnum'] = line_nr}
-
-	local line_diagnostics = vim.diagnostic.get(bufnr, opts)
-	if vim.tbl_isempty(line_diagnostics) then return end
-
-	local diagnostic_message = ""
-	for i, diagnostic in ipairs(line_diagnostics) do
-		diagnostic_message = diagnostic_message .. string.format("%d: %s", i, diagnostic.message or "")
-		print(diagnostic_message)
-		if i ~= #line_diagnostics then
-			diagnostic_message = diagnostic_message .. "\n"
-		end
-	end
-	vim.api.nvim_echo({{diagnostic_message, "Normal"}}, false, {})
-end
-
-vim.cmd [[ autocmd! CursorHold * lua PrintDiagnostics() ]]
+--function PrintDiagnostics(opts, bufnr, line_nr, client_id)
+--	bufnr = bufnr or 0
+--	line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
+--	opts = opts or {['lnum'] = line_nr}
+--
+--	local line_diagnostics = vim.diagnostic.get(bufnr, opts)
+--	if vim.tbl_isempty(line_diagnostics) then return end
+--
+--	local diagnostic_message = ""
+--	for i, diagnostic in ipairs(line_diagnostics) do
+--		diagnostic_message = diagnostic_message .. string.format("%d: %s", i, diagnostic.message or "")
+--		print(diagnostic_message)
+--		if i ~= #line_diagnostics then
+--			diagnostic_message = diagnostic_message .. "\n"
+--		end
+--	end
+--	vim.api.nvim_echo({{diagnostic_message, "Normal"}}, false, {})
+--end
+--
+--vim.cmd [[ autocmd! CursorHold * lua PrintDiagnostics() ]]
 
 
 ------------------------------------------
--- archlinux/dotfiles/.config/nvim/after/plugin/old/nvim-tree.lua
+++ archlinux/dotfiles/.config/nvim/after/plugin/old/nvim-tree.lua
@@ -1,182 +1,182 @@
-local nvimtree_status_ok, nvimtree = pcall(require, "nvim-tree")
-if not nvimtree_status_ok then
-  return
-end
-
-nvimtree.setup { -- BEGIN_DEFAULT_OPTS
-      auto_reload_on_write = true,
-      disable_netrw = false,
-      hijack_cursor = false,
-      hijack_netrw = true,
-      hijack_unnamed_buffer_when_opening = false,
-      sort_by = "name",
-      root_dirs = {},
-      prefer_startup_root = false,
-      sync_root_with_cwd = false,
-      reload_on_bufenter = false,
-      respect_buf_cwd = false,
-      on_attach = "default",
-      remove_keymaps = false,
-      select_prompts = false,
-      view = {
-        centralize_selection = false,
-        cursorline = true,
-        debounce_delay = 15,
-        width = 30,
-        hide_root_folder = false,
-        side = "left",
-        preserve_window_proportions = false,
-        number = false,
-        relativenumber = false,
-        signcolumn = "no",
-        mappings = {
-          custom_only = false,
-          list = {
-            -- user mappings go here
-          },
-        },
-        float = {
-          enable = false,
-          quit_on_focus_loss = true,
-          open_win_config = {
-            relative = "editor",
-            border = "rounded",
-            width = 30,
-            height = 30,
-            row = 1,
-            col = 1,
-          },
-        },
-      },
-      renderer = {
-        add_trailing = false,
-        group_empty = false,
-        highlight_git = false,
-        full_name = false,
-        highlight_opened_files = "none",
-        highlight_modified = "none",
-        root_folder_label = ":~:s?$?/..?",
-        indent_width = 2,
-        indent_markers = {
-          enable = false,
-          inline_arrows = true,
-          icons = {
-            corner = "└",
-            edge = "│",
-            item = "│",
-            bottom = "─",
-            none = " ",
-          },
-        },
-        icons = {
-          webdev_colors = true,
-          git_placement = "before",
-          modified_placement = "after",
-          padding = " ",
-          symlink_arrow = " ➛ ",
-          show = {
-            file = true,
-            folder = true,
-            folder_arrow = true,
-            git = true,
-            modified = true,
-          },
-          glyphs = {
-            default = "",
-            symlink = "",
-            bookmark = "",
-            modified = "●",
-            folder = {
-              arrow_closed = "",
-              arrow_open = "",
-              default = "",
-              open = "",
-              empty = "",
-              empty_open = "",
-              symlink = "",
-              symlink_open = "",
-            },
-            git = {
-              unstaged = "✗",
-              staged = "✓",
-              unmerged = "",
-              renamed = "➜",
-              untracked = "★",
-              deleted = "",
-              ignored = "◌",
-            },
-          },
-        },
-        special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
-        symlink_destination = true,
-      },
-      hijack_directories = {
-        enable = true,
-        auto_open = true,
-      },
-      update_focused_file = {
-        enable = false,
-        update_root = false,
-        ignore_list = {},
-      },
-      system_open = {
-        cmd = "",
-        args = {},
-      },
-      diagnostics = {
-        enable = false,
-        show_on_dirs = false,
-        show_on_open_dirs = true,
-        debounce_delay = 50,
-        severity = {
-          min = vim.diagnostic.severity.HINT,
-          max = vim.diagnostic.severity.ERROR,
-        },
-        icons = {
-          hint = "H",
-          info = "I",
-          warning = "W",
-          error = "E",
-        },
-      },
-      filters = {
-        dotfiles = false,
-        git_clean = false,
-        no_buffer = false,
-        custom = {},
-        exclude = {},
-      },
-      filesystem_watchers = {
-        enable = true,
-        debounce_delay = 50,
-        ignore_dirs = {},
-      },
-      git = {
-        enable = true,
-        ignore = true,
-        show_on_dirs = true,
-        show_on_open_dirs = true,
-        timeout = 400,
-      },
-      modified = {
-        enable = true,
-        show_on_dirs = true,
-        show_on_open_dirs = true,
-      },
-      tab = {
-        sync = {
-          open = true,
-          close = false,
-          ignore = {},
-        },
-      },
-      notify = {
-        threshold = vim.log.levels.INFO,
-      },
-    } -- END_DEFAULT_OPTS
-
-----------------------------------------------------
-----					KEYMAPS						--
-----------------------------------------------------
-local k = vim.keymap.set
-k('n', '<leader>e', vim.cmd.NvimTreeToggle)
+--local nvimtree_status_ok, nvimtree = pcall(require, "nvim-tree")
+--if not nvimtree_status_ok then
+--  return
+--end
+--
+--nvimtree.setup { -- BEGIN_DEFAULT_OPTS
+--      auto_reload_on_write = true,
+--      disable_netrw = false,
+--      hijack_cursor = false,
+--      hijack_netrw = true,
+--      hijack_unnamed_buffer_when_opening = false,
+--      sort_by = "name",
+--      root_dirs = {},
+--      prefer_startup_root = false,
+--      sync_root_with_cwd = false,
+--      reload_on_bufenter = false,
+--      respect_buf_cwd = false,
+--      on_attach = "default",
+--      remove_keymaps = false,
+--      select_prompts = false,
+--      view = {
+--        centralize_selection = false,
+--        cursorline = true,
+--        debounce_delay = 15,
+--        width = 30,
+--        hide_root_folder = false,
+--        side = "left",
+--        preserve_window_proportions = false,
+--        number = false,
+--        relativenumber = false,
+--        signcolumn = "no",
+--        mappings = {
+--          custom_only = false,
+--          list = {
+--            -- user mappings go here
+--          },
+--        },
+--        float = {
+--          enable = false,
+--          quit_on_focus_loss = true,
+--          open_win_config = {
+--            relative = "editor",
+--            border = "rounded",
+--            width = 30,
+--            height = 30,
+--            row = 1,
+--            col = 1,
+--          },
+--        },
+--      },
+--      renderer = {
+--        add_trailing = false,
+--        group_empty = false,
+--        highlight_git = false,
+--        full_name = false,
+--        highlight_opened_files = "none",
+--        highlight_modified = "none",
+--        root_folder_label = ":~:s?$?/..?",
+--        indent_width = 2,
+--        indent_markers = {
+--          enable = false,
+--          inline_arrows = true,
+--          icons = {
+--            corner = "└",
+--            edge = "│",
+--            item = "│",
+--            bottom = "─",
+--            none = " ",
+--          },
+--        },
+--        icons = {
+--          webdev_colors = true,
+--          git_placement = "before",
+--          modified_placement = "after",
+--          padding = " ",
+--          symlink_arrow = " ➛ ",
+--          show = {
+--            file = true,
+--            folder = true,
+--            folder_arrow = true,
+--            git = true,
+--            modified = true,
+--          },
+--          glyphs = {
+--            default = "",
+--            symlink = "",
+--            bookmark = "",
+--            modified = "●",
+--            folder = {
+--              arrow_closed = "",
+--              arrow_open = "",
+--              default = "",
+--              open = "",
+--              empty = "",
+--              empty_open = "",
+--              symlink = "",
+--              symlink_open = "",
+--            },
+--            git = {
+--              unstaged = "✗",
+--              staged = "✓",
+--              unmerged = "",
+--              renamed = "➜",
+--              untracked = "★",
+--              deleted = "",
+--              ignored = "◌",
+--            },
+--          },
+--        },
+--        special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
+--        symlink_destination = true,
+--      },
+--      hijack_directories = {
+--        enable = true,
+--        auto_open = true,
+--      },
+--      update_focused_file = {
+--        enable = false,
+--        update_root = false,
+--        ignore_list = {},
+--      },
+--      system_open = {
+--        cmd = "",
+--        args = {},
+--      },
+--      diagnostics = {
+--        enable = false,
+--        show_on_dirs = false,
+--        show_on_open_dirs = true,
+--        debounce_delay = 50,
+--        severity = {
+--          min = vim.diagnostic.severity.HINT,
+--          max = vim.diagnostic.severity.ERROR,
+--        },
+--        icons = {
+--          hint = "H",
+--          info = "I",
+--          warning = "W",
+--          error = "E",
+--        },
+--      },
+--      filters = {
+--        dotfiles = false,
+--        git_clean = false,
+--        no_buffer = false,
+--        custom = {},
+--        exclude = {},
+--      },
+--      filesystem_watchers = {
+--        enable = true,
+--        debounce_delay = 50,
+--        ignore_dirs = {},
+--      },
+--      git = {
+--        enable = true,
+--        ignore = true,
+--        show_on_dirs = true,
+--        show_on_open_dirs = true,
+--        timeout = 400,
+--      },
+--      modified = {
+--        enable = true,
+--        show_on_dirs = true,
+--        show_on_open_dirs = true,
+--      },
+--      tab = {
+--        sync = {
+--          open = true,
+--          close = false,
+--          ignore = {},
+--        },
+--      },
+--      notify = {
+--        threshold = vim.log.levels.INFO,
+--      },
+--    } -- END_DEFAULT_OPTS
+--
+------------------------------------------------------
+------					KEYMAPS						--
+------------------------------------------------------
+--local k = vim.keymap.set
+--k('n', '<leader>e', vim.cmd.NvimTreeToggle)
