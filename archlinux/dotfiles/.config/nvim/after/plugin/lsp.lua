9e47c5e6d1b18f2a62de404703a74b99247bc7c3
miaou33 <nfauconn@student.42.fr>
2023-05-15 15:26:06 +0200

    some mods

--- archlinux/dotfiles/.config/nvim/after/plugin/lsp.lua
+++ archlinux/dotfiles/.config/nvim/after/plugin/lsp.lua
@@ -81,7 +81,7 @@ vim.diagnostic.config({
 		active = signs, -- show signs
 	},
 	underline = true,
-	update_in_insert = false,
+	update_in_insert = true,
 	severity_sort = false,
 	float = {
 		focusable = true,
@@ -93,8 +93,6 @@ vim.diagnostic.config({
 	},
 })
 
-
-
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 	border = "rounded",
 })
@@ -102,30 +100,6 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
 	border = "rounded",
 })
-
-------------------------------------------
---		DIAG TO MESSAGE AREA			--
-------------------------------------------
---function PrintDiagnostics(opts, bufnr, line_nr, client_id)
---	bufnr = bufnr or 0
---	line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
---	opts = opts or {['lnum'] = line_nr}
---
---	local line_diagnostics = vim.diagnostic.get(bufnr, opts)
---	if vim.tbl_isempty(line_diagnostics) then return end
---
---	local diagnostic_message = ""
---	for i, diagnostic in ipairs(line_diagnostics) do
---		diagnostic_message = diagnostic_message .. string.format("%d: %s", i, diagnostic.message or "")
---		print(diagnostic_message)
---		if i ~= #line_diagnostics then
---			diagnostic_message = diagnostic_message .. "\n"
---		end
---	end
---	vim.api.nvim_echo({{diagnostic_message, "Normal"}}, false, {})
---end
---
---vim.cmd [[ autocmd! CursorHold * lua PrintDiagnostics() ]]
 
 
 ------------------------------------------
@@ -138,7 +112,7 @@ vim.api.nvim_create_autocmd("CursorHold", {
 		local opts = {
 			focusable = false,
 			close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-			border = 'rounded',
+--			border = 'rounded',
 			source = 'always',
 			prefix = ' ',
 			scope = 'cursor',
@@ -211,67 +185,3 @@ local function lsp_keymaps(bufnr)
 end
 
 return M
-		--local lsp = require("lsp-zero")
-		--
-		--lsp.preset("recommended")
-		--
-		--lsp.ensure_installed({
-			--})
-			--
-			---- Fix Undefined global 'vim'
-			--lsp.nvim_workspace()
-			--
-			--
-			--local cmp = require('cmp')
-			--local cmp_select = {behavior = cmp.SelectBehavior.Select}
-			--local cmp_mappings = lsp.defaults.cmp_mappings({
-				--  ['<Up>'] = cmp.mapping.select_prev_item(cmp_select),
-				--  ['<Down>'] = cmp.mapping.select_next_item(cmp_select),
-				--  ["<CR>"] = cmp.mapping.confirm { select = false },
-				--  ['<C-Enter>'] = cmp.mapping.confirm({ select = true }),
-			--  ["<C-Space>"] = cmp.mapping.complete(),
-			--})
-			--
-			--cmp_mappings['<Tab>'] = nil
-			--cmp_mappings['<S-Tab>'] = nil
-			--
-			--lsp.setup_nvim_cmp({
-				--  mapping = cmp_mappings
-				--})
-				--
-				--lsp.set_preferences({
-					--    suggest_lsp_servers = false,
-					--    sign_icons = {
-						--        error = 'E',
-						--        warn = 'W',
-						--        hint = 'H',
-						--        info = 'I'
-						--    }
-						--})
-						--
-						--lsp.on_attach(function(client, bufnr)
-							--  local opts = {buffer = bufnr, remap = false}
-							--  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-							--  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-							--  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-							--  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-							--  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-							--  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-							--  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-							--  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-							--  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-							--  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-							--end)
-							--
-							--lsp.setup()
-							--
-							--vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
-							--
-							--vim.diagnostic.config({
-								--    virtual_text = true,
-								--	underline = false,
-								--	signs = true,
-								--	flycheck = false,
-								--})
-								--
-
--- archlinux/dotfiles/.config/nvim/after/plugin/telescope.lua
+++ archlinux/dotfiles/.config/nvim/after/plugin/telescope.lua
@@ -20,8 +20,8 @@ vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 
 -- current buffer
-vim.keymap.set('n', '<leader>bc', builtin.git_bcommits, {})
-vim.keymap.set('n', '<leader>bs', builtin.current_buffer_fuzzy_find, {})
+--vim.keymap.set('n', '<leader>bc', builtin.git_bcommits, {})
+--vim.keymap.set('n', '<leader>bs', builtin.current_buffer_fuzzy_find, {})
 
 -- registers
 vim.keymap.set('n', '<leader>fr', builtin.registers, {})
