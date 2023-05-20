02c42aa0d75f6917ce0f518df84cab44007798f9
no3 <meow@no3arch>
2023-05-20 17:06:34 +0200

    new colorschemes : base16

--- archlinux/dotfiles/.config/nvim/after/plugin/colorschemes/base16.lua
+++ archlinux/dotfiles/.config/nvim/after/plugin/colorschemes/base16.lua
@@ -1,7 +1,19 @@
 --------------------------------------------------
 --					APATHY						--
 --------------------------------------------------
+<<<<<<< HEAD
 local apathy_status_ok, apathy = pcall(require, "base16")
 if not apathy_status_ok then
   return
 end
+=======
+--local apathy_status_ok, apathy = pcall(require, "base16")
+--if not apathy_status_ok then
+--  return
+--end
+
+--vim.api.nvim_set_var("base16_transparent_background", 1)
+--vim.api.nvim_command('set background=light')
+vim.api.nvim_command('colorscheme apathy')
+
+>>>>>>> 31e804d (new colorschemes : base16)
