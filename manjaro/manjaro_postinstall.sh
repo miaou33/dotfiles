15a0171cae4ffbd030587ac0da9bb58c2f40e079
no3 <nfauconn@student.42.fr>
2023-06-16 17:43:28 +0200

    config script

--- manjaro/manjaro_postinstall.sh
+++ manjaro/manjaro_postinstall.sh
@@ -16,9 +16,4 @@ wget https://git.io/v5Zww -O $HOME"/.config/terminator/plugins/terminator-themes
 
 ssh-keygen
 cat .ssh/id_rsa.pub
-#after adding it to github:
-#git clone git@github.com:nfauconn/CONFiiG.git
-#rm -rf .i3/config && ln -s CONFiiG/manjaro/.i3/config .i3/config
-#cd .config
-#rm -rf terminator && ln -s CONFiiG/manjaro/.config/terminator terminator
-#rm -rf nvim && ln -s CONFiiG/manjaro/.config/nvim nvim
+echo "\nminimal setup ok. please add ssh key to authorized ones in github then exec config.sh" 
