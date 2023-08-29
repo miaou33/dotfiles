9f493f0f2327a27c9057f2a1b01282f5a189b9fb
no3 <nfauconn@student.42.fr>
2023-08-29 15:57:18 +0200

    MOD: updated config.sh

--- manjaro/config.sh
+++ manjaro/config.sh
@@ -1,38 +1,38 @@
-git config --global user.email "nfauconn@student.42.fr"
-git config --global user.name "no3"
+#::::::::::::::::::::::::::::::::::::::::::::::::::::: PACKAGES
 
-git clone git@github.com:nfauconn/dotfiles.git CONFiiG
+yay -S bat exa neovim nvim-packer-git unzip docker docker-buildx
 
-#default apps
-rm .profile && ln -s CONFiiG/manjaro/.profile .profile
-
-#i3
-rm -rf .i3/config && ln -s CONFiiG/manjaro/.i3/config .i3/config
-
-#go to .config dir
-cd .config
-
-#terminator
 yay -S terminator
 pip install requests
 mkdir -p $HOME/.config/terminator/plugins
 wget https://git.io/v5Zww -O $HOME"/.config/terminator/plugins/terminator-themes.py"
-rm -rf terminator && ln -s CONFiiG/manjaro/.config/terminator terminator
+echo "\n\nThemes for terminator installed !!\n\n"
 
-#neovim
-yay -S neovim nvim-packer-git unzip
-rm -rf nvim && ln -s CONFiiG/manjaro/.config/nvim nvim
 
-#go back to home
-cd ..
+#::::::::::::::::::::::::::::::::::::::::::::::::::::: CONFIG
+
+git config --global user.email "nfauconn@student.42.fr"
+git config --global user.name "no3"
+
+cd $HOME
+
+mv $HOME/.profile $HOME/.profile.bak && ln -s $HOME/dotfiles/manjaro/.profile .profile
+mv $HOME/.zshrc $HOME/.zshrc.bak && ln -s $HOME/dotfiles/manjaro/.zshrc .zshrc
+
+cd $HOME/.config
+
+mv $HOME/.i3/config $HOME/.i3/config.bak && cd .i3 && ln -s $HOME/dotfiles/manjaro/.i3/config config 
+mv $HOME/.config/terminator $HOME/.config/terminator.bak && ln -s $HOME/dotfiles/manjaro/.config/terminator terminator
+ln -s $HOME/dotfiles/manjaro/.config/nvim nvim
+
+cd $HOME
 
 #inception
-yay -S docker docker-buildx
 gcl git@github.com:nfauconn/inception.git
 sudo systemctl start docker.service
 sudo systemctl start docker.socket
 sudo systemctl enable docker.service
 sudo systemctl enable docker.socket
-sudo groupadd docker
 sudo usermod -aG docker $USER
+
 echo "install ok. please delog & relog to complete"
