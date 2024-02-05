d4b021ad6f58fcd437204b149eb9ea6ed6a58e82
noe <nfauconn@student.42.fr>
2024-02-05 17:13:26 +0100

    updatedd change theme so it pops up at launch

--- archlinux/dotfiles/.change_theme.sh
+++ archlinux/dotfiles/.change_theme.sh
@@ -5,12 +5,26 @@ WALLPAPER_DARK="/home/noe/Images/gemston2k.png"
 
 GTK_CONFIG_FILE="$HOME/.config/gtk-3.0/settings.ini"
 
-if [ "$1" = "light" ]; then
-	sed -i 's/gtk-theme-name=Adwaita-dark$/gtk-theme-name=Adwaita/' "$GTK_CONFIG_FILE"
-	feh --no-fehbg --bg-fill "$WALLPAPER_LIGHT"
-elif [ "$1" = "dark" ]; then
-	sed -i 's/gtk-theme-name=Adwaita$/gtk-theme-name=Adwaita-dark/' "$GTK_CONFIG_FILE"
-	feh --no-fehbg --bg-fill "$WALLPAPER_DARK"
+if [ -z "$1" ]; then
+    echo "Mood ? :3 (1) light, (2) dark"
+    read theme_selection
+    if [ "$theme_selection" = "1" ]; then
+		sed -i 's/gtk-theme-name=Adwaita-dark$/gtk-theme-name=Adwaita/' "$GTK_CONFIG_FILE"
+		feh --no-fehbg --bg-fill "$WALLPAPER_LIGHT"
+    elif [ "$theme_selection" = "2" ]; then
+		sed -i 's/gtk-theme-name=Adwaita$/gtk-theme-name=Adwaita-dark/' "$GTK_CONFIG_FILE"
+		feh --no-fehbg --bg-fill "$WALLPAPER_DARK"
+    else
+        echo "light or darkkk ?"
+    fi
 else
-	echo "Usage: $0 [light|dark]"
+	if [ "$1" = "light" ]; then
+		sed -i 's/gtk-theme-name=Adwaita-dark$/gtk-theme-name=Adwaita/' "$GTK_CONFIG_FILE"
+		feh --no-fehbg --bg-fill "$WALLPAPER_LIGHT"
+	elif [ "$1" = "dark" ]; then
+		sed -i 's/gtk-theme-name=Adwaita$/gtk-theme-name=Adwaita-dark/' "$GTK_CONFIG_FILE"
+		feh --no-fehbg --bg-fill "$WALLPAPER_DARK"
+	else
+		echo "Usage: $0 [light|dark]"
+	fi
 fi
