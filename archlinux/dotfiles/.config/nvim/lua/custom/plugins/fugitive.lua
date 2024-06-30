7618011998437ed0bf8ca4cb7d7be29e2a793276
noe <nfauconn@student.42.fr>
2024-06-30 10:07:49 +0200

    nvim update to lazy & i3 config

---
+++ archlinux/dotfiles/.config/nvim/lua/custom/plugins/fugitive.lua
@@ -0,0 +1,23 @@
+return {
+  'tpope/vim-fugitive',
+
+  config = function()
+    vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+    vim.keymap.set('n', '<space>gp', ':Git push<CR>')
+
+    --=		Toggle an inline diff of the file under the cursor.
+
+    --s		Stage (add) the file or hunk under the cursor.
+    --u		Unstage (reset) the file or hunk under the cursor.
+    --U		Unstage everything.
+
+    --cc	Create a commit.
+    --ca	Amend the last commit and edit the message.
+    --ce	Amend the last commit without editing the message.
+    --cw	Reword the last commit.
+    --crc	Revert the commit under the cursor.
+
+    --ri	Perform an interactive rebase.
+    --		>Uses ancestor of commit under cursor as upstream if available.
+  end,
+}
